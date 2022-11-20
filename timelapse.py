df5d70a1ea5b30cc65a4b6d3da1ca532af502222
Juan Ramirez Jardua <lch_jramirez@hotmail.com>
2022-11-20 00:27:33 -0300

    enhancements

--- timelapse.py
+++ timelapse.py
@@ -18,8 +18,9 @@ def calcProcessTime(starttime, cur_iter, max_iter):
     finishtime = datetime.datetime.fromtimestamp(finishtime).strftime("%H:%M:%S")  # in time
 
     lefttime = testimated-telapsed  # in seconds
-
-    return (int(cur_iter), int(max_iter), int(telapsed), int(lefttime), finishtime)
+    stelapsed = time.strftime('%Hh %Mm %Ss', time.gmtime(  int(telapsed)))
+    slefttime = time.strftime('%Hh %Mm %Ss', time.gmtime(  int(lefttime)))
+    return (int(cur_iter), int(max_iter), stelapsed,slefttime, finishtime)
 
 
 
@@ -27,12 +28,16 @@ parser= ArgumentParser(description='Timelapse program.')
 parser.add_argument('--fps','-f', dest='fps',help='frames per second timelapse video', default=30, type=int, metavar='fps')
 parser.add_argument('--tlminutes','-t',dest='tlminutes', help='number of minutes you wish to run your timelapse camera', default=3, type=int, metavar='minutes')
 parser.add_argument('--interval','-i',dest='secondsinterval',help='number of seconds delay between each photo taken',default=1, type=int,metavar='seconds')
+parser.add_argument('--resolution','-r',dest='resolution',help='resolution of video',default='1920x1080', type=str, metavar='pixels', choices=['1024x768', '1920x1080','3840x2160'])
+parser.add_argument('--onlyrender',dest='onlyrender',help='Only render, no snapshots', type = str.lower, default='n',choices=['n','y'])
 args=parser.parse_args()
+print(args)
 
 tlminutes = args.tlminutes #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = args.secondsinterval #number of seconds delay between each photo taken
 fps = args.fps #frames per second timelapse video
 numphotos = int((tlminutes*60)/secondsinterval) #number of photos to take
+
 print("number of photos to take = ", numphotos)
 print("fps = ", fps)
 print("seconds interval =",secondsinterval)
@@ -43,11 +48,13 @@ datetimeformat = dateraw.strftime("%Y-%m-%d_%H:%M")
 print("RPi started taking photos for your timelapse at: " + datetimeformat)
 
 camera = PiCamera()
-#camera.resolution = (1024, 768)
-#camera.resolution = (1920,1080)
-camera.resolution = (3840,2160)
 
-system('rm /home/pi/Pictures/*.jpg') #delete all photos in the Pictures folder before timelapse start
+if args.resolution == '1024x768':
+   camera.resolution = (1024, 768)
+elif args.resolution == '1920x1080':
+  camera.resolution = (1920,1080)
+elif args.resolution =='3840x2160':
+  camera.resolution = (3840,2160)
 
 nfotos = 0
 refresh=int(numphotos/10)
@@ -56,24 +63,30 @@ camera.annotate_text_size = 30
 camera.annotate_foreground = Color('black')
 camera.annotate_background = Color('white')
 
-for i in range(numphotos):
-    now = datetime.datetime.now()
-    camera.annotate_text = now.strftime("%d-%m-%Y %H:%M:%S")
-    camera.capture('/home/pi/Pictures/image{0:06d}.jpg'.format(i))
-    time.sleep(secondsinterval)
-    nfotos = nfotos+1
-    if nfotos % refresh == 0 or True:
-        prstime = calcProcessTime(start, nfotos, numphotos)
-        stdout.write("\rphoto:%i of %i elapsed: %s(s), left: %s(s), ETA: %s" % prstime)
-        stdout.flush()
+if args.onlyrender == 'n':
+   system('rm /home/pi/Pictures/*.jpg') #delete all photos in the Pictures folder before timelapse start
+   for i in range(numphotos):
+       now = datetime.datetime.now()
+       camera.annotate_text = now.strftime("%d-%m-%Y %H:%M:%S")
+       camera.capture('/home/pi/Pictures/image{0:06d}.jpg'.format(i))
+       time.sleep(secondsinterval)
+       nfotos = nfotos+1
+       if nfotos % refresh == 0 or True:
+         prstime = calcProcessTime(start, nfotos, numphotos)
+         stdout.write("\rphoto:%i of %i elapsed: %s, left: %s, ETA: %s" % prstime)
+         stdout.flush()
 
-stdout.write("\n")
-
-print("Done taking photos.")
+   stdout.write("\n")
+   print("Done taking photos.")
+ 
 print("Please standby as your timelapse video is created.")
 
-#system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
-#system('ffmpeg -r {} -f image2 -s 1920x1080 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
-system('ffmpeg -r {} -f image2 -s 3840x2160 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
+if args.resolution == '1024x668':
+   system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
+elif args.resolution == '1920x1080':
+   system('ffmpeg -r {} -f image2 -s 1920x1080 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
+elif args.resolution =='3840x2160':
+   system('ffmpeg -r {} -f image2 -s 3840x2160 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
+
 #system('rm /home/pi/Pictures/*.jpg')
 print('Timelapse video is complete. Video saved as /home/pi/Videos/{}.mp4'.format(datetimeformat))
